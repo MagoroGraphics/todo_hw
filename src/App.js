6e3112b463ed1b2ec9660af49a124444116ce027
user <user@users-MacBook-Pro.local>
2022-05-09 16:14:50 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
+
 
 function App() {
+const [items, setItems] = useState([
+  {task: "Clean windows", isHighPriority: "low"},
+  {task: "Check car brakes", isHighPriority: "high"}
+])
+
+const [newItem, setNewItem] = useState();
+const [priorityButton, setPriorityButton] = useState();
+
+const saveNewItem = (event) => {
+  event.preventDefault()
+  const copyItem = [... items, {task : newItem, isHighPriority: priorityButton}]
+  setItems(copyItem)
+  setNewItem("")
+}
+
+const itemNodes = items.map((item, index) => { 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <li key={index} className={item.isHighPriority === "high" ? "high" : "low"}>
+        
+      <span>{item.task}</span>
+       
+      </li> 
+  )
+})
+
+const handleItemInput = (event) => {
+  setNewItem(event.target.value)
+}
+
+const handlePriority = (priority) => {
+  setPriorityButton(priority.target.value)
+}
+
+  return (
+    <>
+    <h1>To do list</h1>
+
+    <form onSubmit={saveNewItem}>
+      
+
+      <input type="radio" id="high" name="priorityButton" value= "high" onChange={handlePriority} ></input>
+      <label for="high">High</label> 
+
+      <input type="radio" id="low" name="priorityButton" value="low" onChange={handlePriority}></input>
+      <label for="low">Low</label> 
+
+      <input id="new-item" type="text" onChange={handleItemInput} value={newItem} /> 
+
+      <input type="submit" value='Save new task' />
+      
+     
+    </form>
+    
+    <ul>
+      {itemNodes}
+    </ul>
+    </>
   );
 }
 
